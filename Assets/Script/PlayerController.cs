c5e8a00cd77f2b321b6e1dae95a51a4ea3235bd6
KaTSukiFireStar <gonin.flavien@gmail.com>
2026-05-11 10:49:56 +0200

    feature(Player) : Add input action for player movement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        private InputActions _input;
+        private Rigidbody2D _rb;
+
+        [SerializeField]
+        private Vector2 speed;
+    
+        private void Awake()
+        {
+            _input = new InputActions();
+            _input.Player.Enable();
+            
+            _rb = GetComponent<Rigidbody2D>();
+        }
+    
+        private void Update()
+        {
+            var move = _input.Player.Move.ReadValue<Vector2>();
+            
+            _rb.MovePosition(_rb.position + move * speed * Time.deltaTime);
+        }
+    }
+}
+
