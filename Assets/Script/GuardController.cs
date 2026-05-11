896561454b29ac7920af205bc2f614643cb1c678
KaTSukiFireStar <gonin.flavien@gmail.com>
2026-05-11 11:39:07 +0200

    feature(Guard) : Add minimal recognition for guard with math

--- Assets/Script/GuardController.cs
+++ Assets/Script/GuardController.cs
@@ -1,11 +1,33 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GuardController : MonoBehaviour
 {
+    [SerializeField]
+    private Transform playerPos;
+
+    [SerializeField]
+    private float _recognitionDistance = 5;
+
+    [SerializeField]
+    private float _recognitionAngle = 15;
     
-    
+    private void Update()
+    {
+        float angle = Mathf.Acos(Vector3.Dot(Vector3.Normalize(transform.up), Vector3.Normalize(transform.position - playerPos.position)));
+        angle = angle * 180 / Mathf.PI;
+        if (angle < _recognitionAngle)
+        {
+            if (Vector3.Distance(transform.position, playerPos.position) < _recognitionDistance)
+            {
+                Trigger(playerPos.position);
+            }
+        }
+    }
+
     public void Trigger(Vector2 pos)
     {
-        Debug.Log("Je te vois");
+        Debug.Log("Je te vois par math");
     }
 }
