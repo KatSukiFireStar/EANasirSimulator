12de6cd8535ccf83a327f95bea589f63b64197dd
KaTSukiFireStar <gonin.flavien@gmail.com>
2026-05-16 15:19:38 +0200

    feature(Player) : Add Interaction for player

---
+++ Assets/Script/IInteractable.cs
@@ -0,0 +1,8 @@
+public interface IInteractable
+{
+    /// <summary>
+    /// Interact with object
+    /// </summary>
+    /// <returns>Copper to return after interaction</returns>
+    public int Interact();
+}
--- Assets/Script/PlayerAttackCollider.cs
+++ Assets/Script/PlayerAttackCollider.cs
@@ -10,6 +10,10 @@ public class PlayerAttackCollider : MonoBehaviour
 		{
 			EventManager.InvokeEvent("AttackTriggerBox", obj);
 		}
+		else if (other.TryGetComponent(out IInteractable inter))
+		{
+			EventManager.InvokeEvent("InteractTriggerBox", obj);
+		}
 	}
 
 	private void OnTriggerExit2D(Collider2D other)
@@ -17,6 +21,10 @@ public class PlayerAttackCollider : MonoBehaviour
 		if (other.TryGetComponent(out IAttackable obj))
 		{
 			EventManager.InvokeEvent("RemoveTriggerBox");
+		}
+		else if (other.TryGetComponent(out IInteractable inter))
+		{
+			EventManager.InvokeEvent("RemoveInteractTriggerBox");
 		}
 	}
 }
