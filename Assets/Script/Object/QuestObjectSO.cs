f27ef36f03381dfb2a428112ef56c83efb7cf7ec
KaTSukiFireStar <gonin.flavien@gmail.com>
2026-05-18 17:35:31 +0200

    feature(QuestObject) : Add QuestObject for player

---
+++ Assets/Script/Object/QuestObjectSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace EANasir.Object
+{
+	[CreateAssetMenu(fileName = "QuestObjectSO", menuName = "Scriptable Objects/QuestObjectSO")]
+
+	public class QuestObjectSO : ScriptableObject
+	{
+		public new string name;
+	}
+
+}
